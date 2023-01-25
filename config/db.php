0478f51458330730a3f329e302b9a4937f1c4d98
Thunderbird93 <antircristian@gmail.com>
2023-01-25 17:02:15 +0100

    image

--- config/db.php
+++ config/db.php
@@ -4,113 +4,136 @@ return [
     'artists' => [
         [
             'id'   => 1,
-            'name' => 'Leonardo Da Vinci'
+            'name' => 'Leonardo Da Vinci',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Leonardo_self.jpg'
         ],
         [
             'id'   => 2,
-            'name' => 'Vincent van Gogh'
+            'name' => 'Vincent van Gogh',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/VanGogh_1887_Selbstbildnis.jpg/1280px-VanGogh_1887_Selbstbildnis.jpg'
         ],
         [
             'id'   => 3,
-            'name' => 'Pablo Picasso'
+            'name' => 'Pablo Picasso',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/9/98/Pablo_picasso_1.jpg'
         ],
         [
             'id'   => 4,
-            'name' => 'Botticelli'
+            'name' => 'Botticelli',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Sandro_Botticelli_083.jpg'
         ],
         [
             'id'   => 5,
-            'name' => 'Caravaggio'
+            'name' => 'Caravaggio',
+            'image' => 'https://scontent.fmxp6-1.fna.fbcdn.net/v/t1.18169-9/377707_10152563704985304_718326929_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=bxrhjuiS0KQAX9DYGIV&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfCSVSxpXYNRKya33dEYMVtck8Bv1drreES0GoH0FEXOlA&oe=63F8B79F'
         ],
         [
             'id'   => 6,
-            'name' => 'Michelangelo'
+            'name' => 'Michelangelo',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/0/02/Michelangelo_Daniele_da_Volterra_%28dettaglio%29.jpg'
         ],
         [
             'id'   => 7,
-            'name' => 'Paul Gauguin'
+            'name' => 'Paul Gauguin',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Gauguin_portrait_1889.JPG'
         ],
         [
             'id'   => 8,
-            'name' => 'Pierre-Auguste Renoir'
+            'name' => 'Pierre-Auguste Renoir',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/6/64/Fr%C3%A9d%C3%A9ric_Bazille_-_Renoir.jpg'
         ],
         [
             'id'   => 9,
-            'name' => 'Salvador Dalì'
+            'name' => 'Salvador Dalì',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/2/27/Salvador_Dali_NYWTS.jpg'
         ]
     ],
     'museums' => [
         [
             'id'     => 1,
             'name'   => 'National Gallery',
-            'nation' => 'Inghilterra'
+            'nation' => 'Inghilterra',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/d/de/Galer%C3%ADa_Nacional%2C_Londres%2C_Inglaterra%2C_2014-08-07%2C_DD_035.JPG'
         ],
         [
             'id'     => 2,
             'name'   => 'Cenacolo',
-            'nation' => 'Italia'
+            'nation' => 'Italia',
+            'image' => 'https://museilombardia.cultura.gov.it/wp-content/uploads/2018/02/Milano-Cenacolo-Vinciano_100717-3384-copia-1-1024x725.jpg'
         ],
         [
             'id'     => 3,
             'name'   => 'Louvre',
-            'nation' => 'Francia'
+            'nation' => 'Francia',
+            'image' => 'https://cdn.getyourguide.com/img/tour/5d4c31d4123d3.jpeg/98.jpg'
         ],
         [
             'id'     => 4,
             'name'   => 'MoMa New York',
-            'nation' => 'Stati Uniti'
+            'nation' => 'Stati Uniti',
+            'image' => 'https://www.moma.org/d/p/sa/maximum/moma_renovation_and_expansion.jpg'
         ],
         [
             'id'     => 5,
             'name'   => 'Galleria degli Uffizi',
-            'nation' => 'Italia'
+            'nation' => 'Italia',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/d/d7/Florence%2C_Italy_-_panoramio_%28125%29.jpg'
         ],
         [
             'id'     => 6,
             'name'   => 'Museo d\'Orsay',
-            'nation' => 'Francia'
+            'nation' => 'Francia',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Mus%C3%A9e_d%27Orsay%2C_North-West_view%2C_Paris_7e_140402.jpg'
         ],
         [
             'id'     => 7,
             'name'   => 'Museo Nacional Centro e Arte Reina Sofia',
-            'nation' => 'Spagna'
+            'nation' => 'Spagna',
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/9/97/MNCARS_05.jpg'
         ]
     ],
     'artworks' => [
         [
             'artist' => 'Leonardo Da Vinci',
             'name'      => 'Gioconda',
-            'year'      => 1503
+            'year'      => 1503,
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
         ],
         [
             'artist' => 'Leonardo Da Vinci',
             'name'      => 'La vergine delle rocce',
-            'year'      => 1483
+            'year'      => 1483,
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Leonardo_Da_Vinci_-_Vergine_delle_Rocce_%28Louvre%29.jpg'
         ],
         [
             'artist' => 'Vincent van Gogh',
             'name'      => 'Notte Stellata',
-            'year'      => 1889
+            'year'      => 1889,
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1920px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'
         ],
         [
             'artist' => 'Vincent van Gogh',
             'name'      => 'Girasoli',
-            'year'      => 1888
+            'year'      => 1888,
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Tournesols_van_gogh_echantillon.jpg'
         ],
         [
             'artist' => 'Botticelli',
             'name'      => 'Nascita di Venere',
-            'year'      => 1486
+            'year'      => 1486,
+            'image' => 'https://it.wikipedia.org/wiki/Nascita_di_Venere#/media/File:Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg'
         ],
         [
             'artist' => 'Botticelli',
             'name'      => 'Natività mistica',
-            'year'      => 1501
+            'year'      => 1501,
+            'image' => 'https://upload.wikimedia.org/wikipedia/commons/c/c5/The_Mystical_Nativity.jpg'
         ],
         [
             'artist' => 'Salvador Dalì',
             'name'      => 'La persistenza della memoria',
-            'year'      => 1931
+            'year'      => 1931,
+            'image' => 'https://www.scuola-e-cultura.it/wp-content/uploads/2022/02/la-persistenza-della-memoria-2.jpg'
         ],
     ]
 ];
